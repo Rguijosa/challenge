e1bb3ef4974e4a5366778e2fe4723c9a633e238f
Rebeca Guijosa <113552170+Rguijosa@users.noreply.github.com>
2022-09-20 20:16:47 +0000

    .

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #jose, rebeca, katherine 
 
 
-Paragraph = "I pledge allegiance to the Flag of the United States of America, and to the Republic for which it stands, one Nation under God, indivisible, with liberty and justice for all."
+Paragraph = "I pledge allegiance to the Flag of the United States of America, and to the Republic for which it stands, one Nation under God, indivisible, with liberty and justice for all"
+
+ask1= print(input("Choose 1 letter from the Alphabet:   "))
+ask2= print(input("Choose 1 letter from the Alphabet:   "))
+ask3= print(input("Choose 1 letter from the Alphabet:   "))
 
 
 
-lst = []
-  
-# number of elements as input
-n = int(input("Choose a random letter: "))
-# Ask1= print(input("Choose 1 letter from the Alphabet:   "))
-# Ask2= print(input("Choose 1 letter from the Alphabet:   "))
-# Ask3= print(input("Choose 1 letter from the Alphabet:   "))
 
-letters = ["Ask1", "Ask2", "Ask3"]
-print(letters.count("Ask1"))
-print(letters.count("Ask2"))
-print(letters.count("Ask3"))
+letters = [input("ask1"), input("ask2"), input("ask3")]
+print(letters)
+
+
